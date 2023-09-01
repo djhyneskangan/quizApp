8819fd87067ec471ea2f5e9d6b43a9a03cc3ebfc
djhyneskangan <200020659@student.kangan.edu.au>
2023-09-01 16:41:21 +1000

    Changes to allow selected answer to change colour

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,26 @@
 <script>
 	export let data;
 	let currentQuestionIndex = 0;
+	let selectedAnswerIndex = -1;
 	console.log(data);
 
 	function goToNextQuestion() {
 		if (currentQuestionIndex < data.questions.length - 1) {
 			currentQuestionIndex++;
+			selectedAnswerIndex = -1;
 		}
 	}
 
 	function goToPreviousQuestion() {
-		if (currentQuestionIndex < data.questions.length + 1) {
+		if (currentQuestionIndex > 0) {
 			currentQuestionIndex--;
+			selectedAnswerIndex = -1;
 		}
+	}
+
+	// @ts-ignore
+	function selectAnswer(index) {
+		selectedAnswerIndex = index;
 	}
 </script>
 
@@ -27,16 +35,31 @@
 	</div>
 
 	<div class="grid grid-cols-2 gap-4 py-5">
-		{#each data.questions[currentQuestionIndex]?.answers as answer}
-			<button class="bg-[#bae6fd]/80 py-4 px-6 rounded">{answer}</button>
+		{#each data.questions[currentQuestionIndex]?.answers as answer, index}
+			<button
+				class="bg-[#bae6fd]/80 py-4 px-6 rounded
+				   {index === selectedAnswerIndex ? 'selected-answer' : ''}"
+				on:click={() => selectAnswer(index)}
+			>
+				{answer}
+			</button>
 		{/each}
 	</div>
 
-	<button class="bg-[#bae6fd]/80 py-4 px-6 rounded mt-4" on:click={goToNextQuestion}>
-		Next Question
-	</button>
+	<div class="grid grid-rows-1 grid-cols-2 gap-4">
+		<button class="bg-[#bae6fd]/80 py-4 px-6 rounded mt-4" on:click={goToPreviousQuestion}>
+			Previous Question
+		</button>
 
-	<button class="bg-[#bae6fd]/80 py-4 px-6 rounded mt-4" on:click={goToPreviousQuestion}>
-		Previous Question
-	</button>
+		<button class="bg-[#bae6fd]/80 py-4 px-6 rounded mt-4" on:click={goToNextQuestion}>
+			Next Question
+		</button>
+	</div>
+
+	<style>
+		.selected-answer {
+			background-color: #003366; /* Change this to the desired darker blue color */
+			color: white;
+		}
+	</style>
 </body>
